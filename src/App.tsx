0884277d498d1a71caff4c10814473a75072cccc
ricardocisco <richardrulex@gmail.com>
2024-05-13 13:21:58 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./GlobalStyle";
+import AppRoutes from "./router/AppRoutes";
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
